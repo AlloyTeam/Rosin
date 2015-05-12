a0b936b157f7cdae728016d821a3d7733b5fe088
sfesh2012 <sfesh@163.com>
2015-05-13 00:03:11 +0800

    重新规划项目目录结构

--- Rosin/readme.txt
+++
@@ -1,5 +0,0 @@
-Rosin开发说明
-
-1、首次开发，运行RosinDev.exe文件，帮助拷贝配置xml、拦截脚本js、Rosin.dll以及依赖dll到Fiddler安装目录；
-2、非首次开发运行RosinDev.exe，将会覆盖掉之前的文件；
-3、开发时build，工程会自动拷贝编译生成的Rosin.dll文件到Fiddler Scripts目录下；
---
+++ RosinForFiddler4/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fiddler;
+
+namespace AlloyTeam.Rosin
+{
+    public class Class1: IAutoTamper
+    {
+        public void AutoTamperRequestAfter(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AutoTamperRequestBefore(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AutoTamperResponseAfter(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AutoTamperResponseBefore(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBeforeReturningError(Session oSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBeforeUnload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnLoad()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
